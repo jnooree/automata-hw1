98c66d4a3126fe9e359399b788bd28068e2727d1
jnooree <jnooree@snu.ac.kr>
2022-09-30 17:11:12 +0900

    Fix bug in run_test.sh

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eu
 
-template="${1:-test/test.in}"
-suffix="${2:-.debug}"
+template="${1-test/test.in}"
+suffix="${2-.debug}"
 
 scripts/cleanup.sh "$(dirname "$template")" &>/dev/null
 scripts/preprocess.py "$template"
