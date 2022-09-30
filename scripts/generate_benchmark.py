dbeee9146fa879e80d29292049ef4fc0c70693a6
jnooree <jnooree@snu.ac.kr>
2022-09-30 15:14:51 +0900

    Update benchmark files

--- scripts/generate_benchmark.py
+++ scripts/generate_benchmark.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import random
 
 
@@ -12,7 +13,9 @@ def main():
         ''.join(random.choices(alphabets, k=size)) for size in selected_sizes]
     results = ["yes" if size % 3 == 0 else "no" for size in selected_sizes]
 
-    with open("benchmark/benchmark.in", "a") as f:
+    os.makedirs("benchmark", exist_ok=True)
+    with open("benchmark/benchmark.in", "w") as f:
+        f.write("((0+1)(0+1)(0+1))*" * 10 + "\n")
         f.write(",".join(inputs) + "\n")
         f.write(",".join(results) + "\n")
 
